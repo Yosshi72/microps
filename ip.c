c1a85b0dc9599aa00826c607afd4851a63941ce2
Yosshi72 <faily.nappy.yshk@gmail.com>
2023-12-29 05:10:55 +0000

    step6: input and verify ip_datagram

--- ip.c
+++ ip.c
@@ -1,15 +1,139 @@
+#include <stdio.h>
 #include <stdint.h>
 #include <stddef.h>
+#include <stdlib.h>
 
 #include "util.h"
 #include "net.h"
 #include "ip.h"
 
-static void
-ip_input(const uint8_t *data, size_t len, struct net_device *dev)
+struct ip_hdr { // IP header
+    uint8_t vhl; // versionとheader長合わせて8bitで扱う
+    uint8_t tos;
+    uint16_t total;
+    uint16_t id;
+    uint16_t offset; // flagとfragment_offsetを合わせて16bitで扱う
+    uint8_t ttl;
+    uint8_t protocol;
+    uint16_t sum;
+    ip_addr_t src; // ip address：ip_addr_tを使う
+    ip_addr_t dst;
+    uint8_t options[]; // 可変長なのでflexible_array_memberとする
+};
+
+const ip_addr_t IP_ADDR_ANY       = 0x00000000; /* 0.0.0.0 */
+const ip_addr_t IP_ADDR_BROADCAST = 0xffffffff; /* 255.255.255.255 */
+
+int
+ip_addr_pton(const char *p, ip_addr_t *n) // ip addressをstr->binary(ip_addr_t)と変換
 {
+    char *sp, *ep;
+    int idx;
+    long ret;
+
+    sp = (char *)p;
+    for (idx = 0; idx < 4; idx++) {
+        ret = strtol(sp, &ep, 10);
+        if (ret < 0 || ret > 255) {
+            return -1;
+        }
+        if (ep == sp) {
+            return -1;
+        }
+        if ((idx == 3 && *ep != '\0') || (idx != 3 && *ep != '.')) {
+            return -1;
+        }
+        ((uint8_t *)n)[idx] = ret;
+        sp = ep + 1;
+    }
+    return 0;
+}
+
+char *
+ip_addr_ntop(ip_addr_t n, char *p, size_t size) // ip addressをbinary(ip_addr_t)->strと変換
+{
+    uint8_t *u8;
+
+    u8 = (uint8_t *)&n;
+    snprintf(p, size, "%d.%d.%d.%d", u8[0], u8[1], u8[2], u8[3]);
+    return p;
+}
+
+static void
+ip_dump(const uint8_t *data, size_t len) // debug出力をする
+{
+    struct ip_hdr *hdr;
+    uint8_t v, hl, hlen;
+    uint16_t total, offset;
+    char addr[IP_ADDR_STR_LEN];
+
+    flockfile(stderr);
+    hdr = (struct ip_hdr*)data;
+    v = (hdr->vhl & 0xf0) >>4; // versionはvhlの上位4bit
+    hl = (hdr->vhl & 0x0f); // header_lenはvhlの下位4bit
+    hlen = hl <<2; // header_lenを32bit単位から8bit単位に変換
+    fprintf(stderr, "       vhl: 0x%02x [v:%u, hl: %u (%u)]\n", hdr->vhl, v, hl, hlen);
+    fprintf(stderr, "       tos: 0x%02x\n", hdr->tos);
+
+    total = ntoh16(hdr->total);
+    fprintf(stderr, "     total: %u (payload: %u)\n", total, total - hlen); // payloadの長さ：total_len - header_len
+    fprintf(stderr, "        id: %u\n", hdr->id);
+    
+    offset = ntoh16(hdr->offset);
+    fprintf(stderr, "    offset: 0x%04x [flags=%x, offset=%u]\n", offset, (offset & 0xe00) >>13, offset & 0x1fff); // offset：上位3bit, flags：下位13bit
+    fprintf(stderr, "       ttl: %u\n", hdr->ttl);
+    fprintf(stderr, "   protocol: %u\n", hdr->protocol);
+    fprintf(stderr, "       sum: 0x%04x\n", hdr->sum);
+    fprintf(stderr, "       src: %s\n", ip_addr_ntop(hdr->src, addr, sizeof(addr))); // ip addressをbinary->str
+    fprintf(stderr, "       dst: %s\n", ip_addr_ntop(hdr->dst, addr, sizeof(addr)));
+#ifdef HEXDUMP
+    hexdump(stderr, data, len);
+#endif
+    funlockfile(stderr);
+}
+
+static void
+ip_input(const uint8_t *data, size_t len, struct net_device *dev) // ip_headerの検証
+{
+    struct ip_hdr *hdr;
+    uint8_t v;
+    uint16_t hlen, total, offset;
+
+    if (len < IP_HDR_SIZE_MIN) { // input_dataの長さがip headerの最小サイズより小さい
+        errorf("too short");
+        return;
+    }
+    hdr = (struct ip_hdr*) data; // input_dataをip_hder構造体のpointerにcast
+
+    v = (hdr->vhl & 0xf0) >>4;
+    if (v != IP_VERSION_IPV4) { // versionがIPv4と一致しない
+        errorf("ip version error: v=%u", v);
+        return;
+    }
+
+    hlen = (hdr->vhl & 0x0f) <<2;
+    if (len < hlen) { // input_dataの長さがheader_lenより小さい
+        errorf("header length error: len=%zu < hlen=%u", len, hlen);
+        return;
+    }
+
+    total = ntoh16(hdr->total);
+    if (len < total) { // input_dataの長さがtotal_lenより小さい
+        errorf("total length error; len=%zu < total=%u", len, total);
+        return;
+    }
+
+    if (cksum16((uint16_t *)hdr, hlen, 0) != 0) { // checksumの検証
+        errorf("checksum error: sum=0x%04x, verify = 0x%04x", ntoh16(hdr->sum), ntoh16(cksum16((uint16_t *)hdr, hlen, -hdr->sum)));
+        return;
+    }
+    offset = ntoh16(hdr->offset); // fragmentがあるかをチェック
+    if (offset & 0x2000 || offset & 0x1fff) { // MF bitが立っている or fragment_offsetに値がある
+        errorf("fragments does not support");
+        return;
+    }
     debugf("dev=%s, len=%zu", dev->name, len); // 入力関数が呼び出されたことだけわかればいい
-    debugdump(data, len);
+    ip_dump(data, len);
 }
 
 int
